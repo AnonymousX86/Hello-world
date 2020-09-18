366648a66fc96e02f5389e55ad0eb429258c643d
Jakub <jakub.suchenek.25@gmail.com>
2020-09-18 15:58:39 +0200

    Comments and 2 more ways

--- hello.js
+++ hello.js
@@ -1,1 +1,12 @@
-console.log("Hello world!")
+// Write in console
+console.log("Hello world!");
+
+// Write in document (it overwrites any HTML document's data)
+document.write("Hello world!");
+
+// Create "span" tag with text (this one does not overwrites document, but appends it)
+const myTag = document.createElement("span");
+myTag.textContent = "Hello world!";
+document.body.appendChild(myTag);
+
+// Execute in browser console (check F12)
